f252909df5f390a6b5821e5a5cf87a7d91d56fcc
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-18 12:36:33 +0100

    Import opportunities dashboard
    The opportunites dashboard for a supplier shows them their responses to
    opportunities. It makes sense to move it to the brief responses frontend
    app.
    
    The url is slightly different to where it was before, so a 301 redirect
    will be put in place in the supplier app to point to this new url.
    
    This means we can also get rid of one of the external links.

---
+++ app/main/views/frameworks.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from flask import render_template, abort
+from flask_login import current_user
+from dmapiclient import APIError
+from ... import data_api_client
+from ...main import main
+from ..helpers import login_required
+
+
+@main.route('/frameworks/<framework_slug>', methods=['GET'])
+@login_required
+def opportunities_dashboard(framework_slug):
+    try:
+        framework = data_api_client.get_framework(slug=framework_slug)['frameworks']
+        supplier_framework = data_api_client.get_supplier_framework_info(
+            supplier_id=current_user.supplier_id,
+            framework_slug=framework['slug']
+        )['frameworkInterest']
+    except APIError as e:
+        abort(e.status_code)
+    if not (framework['framework'] == 'digital-outcomes-and-specialists' and supplier_framework['onFramework']):
+        abort(404)
+    opportunities = data_api_client.find_brief_responses(
+        supplier_id=current_user.supplier_id, framework=framework_slug
+    )['briefResponses']
+
+    return render_template(
+        "frameworks/opportunities_dashboard.html",
+        framework=framework,
+        completed=opportunities
+    ), 200
