af6a4069c1a7623d2c482bda02d9dba70ff856bf
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-01-17 17:09:24 +0000

    Adds in /help page
    bump to 31.1.0
    
    adds in help route

--- app/external/views/external.py
+++ app/external/views/external.py
@@ -11,3 +11,8 @@ def dashboard():
 @external.route('/<string:framework_framework>/opportunities/<int:brief_id>/')
 def buyer_frontend_get_brief_by_id(framework_framework, brief_id):
     raise NotImplementedError()
+
+
+@external.route('/help')
+def help():
+    raise NotImplementedError()
