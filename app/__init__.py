9e6d6b85cd6477970f66b963c9a9fe611afac24b
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-04-19 13:58:10 +0100

    use application CSRF error handler

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import re
 
 from flask import Flask, request, redirect, session, abort
 from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect, CSRFError
 
 import dmapiclient
 from dmutils import init_app, flask_featureflags
@@ -47,7 +47,7 @@ def create_app(config_name):
 
     csrf.init_app(application)
 
-    @csrf.error_handler
+    @application.errorhandler(CSRFError)
     def csrf_handler(reason):
         if 'user_id' not in session:
             application.logger.info(
