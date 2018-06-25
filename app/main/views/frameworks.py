fc903d9ee9bc0e967cbc1f1067a12381b7c99361
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-06-25 15:09:40 +0100

    Include draft applications on dashboard

--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -6,6 +6,8 @@ from dmapiclient import APIError
 from ... import data_api_client
 from ...main import main
 
+BRIEF_RESPONSE_STATUSES = ['draft', 'submitted', 'pending-awarded', 'awarded']
+
 
 @main.route('/frameworks/<framework_slug>', methods=['GET'])
 def opportunities_dashboard(framework_slug):
@@ -20,11 +22,20 @@ def opportunities_dashboard(framework_slug):
     if not (framework['framework'] == 'digital-outcomes-and-specialists' and supplier_framework['onFramework']):
         abort(404)
     opportunities = data_api_client.find_brief_responses(
-        supplier_id=current_user.supplier_id, framework=framework_slug
+        supplier_id=current_user.supplier_id, framework=framework_slug, status=",".join(BRIEF_RESPONSE_STATUSES)
     )['briefResponses']
+
+    # Split into two tables by status
+    drafts, completed = [], []
+    for opportunity in opportunities:
+        if opportunity['status'] == 'draft':
+            drafts.append(opportunity)
+        else:
+            completed.append(opportunity)
 
     return render_template(
         "frameworks/opportunities_dashboard.html",
         framework=framework,
-        completed=opportunities
+        completed=completed,
+        drafts=drafts,
     ), 200
