c576eb0f7470160d0007eb2c17e8825b0c316dcc
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-08-02 10:54:38 +0100

    content loader: implement this as a thread/context-local
    the content loader isn't threadsafe, so the easiest avenue towards thread-
    enabling an app is probably to supply each thread/context with its own
    copy of the content loader. rather than constructing a new ContentLoader
    from scratch for each thread, allow the initial importing process to
    construct a "master" ContentLoader which we seal in a closure. the only
    things that can escape this closure are deep copies which should not share
    any mutable objects with each other and therefore be sufficiently
    independent.
    
    each thread will make its own deep copy of this master content loader when
    it first needs one - a comparatively cheap operation

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,30 +1,57 @@
+from copy import deepcopy
 from functools import partial
+
 from flask import Blueprint
 from dmcontent.content_loader import ContentLoader
 from dmutils.access_control import require_login
+from dmutils.timing import logged_duration
+from werkzeug.local import Local, LocalProxy
 
 main = Blueprint('main', __name__)
 public = Blueprint('public', __name__)  # Supplier login not required
 
-content_loader = ContentLoader('app/content')
+# we use our own Local for objects we explicitly want to be able to retain between requests but shouldn't
+# share a common object between concurrent threads/contexts
+_local = Local()
 
-content_loader.load_manifest('digital-outcomes-and-specialists', 'briefs', 'edit_brief')
-content_loader.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'legacy_edit_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'edit_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'legacy_display_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'display_brief_response')
 
-content_loader.load_manifest('digital-outcomes-and-specialists-2', 'briefs', 'edit_brief')
-content_loader.load_manifest('digital-outcomes-and-specialists-2', 'brief-responses', 'edit_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists-2', 'brief-responses', 'display_brief_response')
+def _make_content_loader_factory():
+    master_cl = ContentLoader('app/content')
 
-content_loader.load_manifest('digital-outcomes-and-specialists-3', 'briefs', 'edit_brief')
-content_loader.load_manifest('digital-outcomes-and-specialists-3', 'brief-responses', 'edit_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists-3', 'brief-responses', 'display_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists', 'briefs', 'edit_brief')
+    master_cl.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'legacy_edit_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'edit_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'legacy_display_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'display_brief_response')
 
-content_loader.load_manifest('digital-outcomes-and-specialists-4', 'briefs', 'edit_brief')
-content_loader.load_manifest('digital-outcomes-and-specialists-4', 'brief-responses', 'edit_brief_response')
-content_loader.load_manifest('digital-outcomes-and-specialists-4', 'brief-responses', 'display_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists-2', 'briefs', 'edit_brief')
+    master_cl.load_manifest('digital-outcomes-and-specialists-2', 'brief-responses', 'edit_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists-2', 'brief-responses', 'display_brief_response')
+
+    master_cl.load_manifest('digital-outcomes-and-specialists-3', 'briefs', 'edit_brief')
+    master_cl.load_manifest('digital-outcomes-and-specialists-3', 'brief-responses', 'edit_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists-3', 'brief-responses', 'display_brief_response')
+
+    master_cl.load_manifest('digital-outcomes-and-specialists-4', 'briefs', 'edit_brief')
+    master_cl.load_manifest('digital-outcomes-and-specialists-4', 'brief-responses', 'edit_brief_response')
+    master_cl.load_manifest('digital-outcomes-and-specialists-4', 'brief-responses', 'display_brief_response')
+
+    # seal master_cl in a closure by returning a function which will only ever return an independent copy of it
+    return lambda: deepcopy(master_cl)
+
+
+_content_loader_factory = _make_content_loader_factory()
+
+
+@logged_duration(message="Spent {duration_real}s in get_content_loader")
+def get_content_loader():
+    if not hasattr(_local, "content_loader"):
+        _local.content_loader = _content_loader_factory()
+    return _local.content_loader
+
+
+content_loader = LocalProxy(get_content_loader)
+
 
 main.before_request(partial(require_login, role='supplier'))
 
